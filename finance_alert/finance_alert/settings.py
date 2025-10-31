2b56f75ca93f960b7f996de8849768e210075a83
Allan <nabuleallan1@gmail.com>
2025-10-31 18:13:49 +0300

    updated requirements.txt

--- finance_alert/finance_alert/settings.py
+++ finance_alert/finance_alert/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-replace-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,https://notifier-dpvz.onrender.com').split(',')
 
 
 # Application definition
